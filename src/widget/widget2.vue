c7d6fdcb5be72fb5d4435fdf20bdee14a33bbaad
fym <373788426@qq.com>
2020-07-29 14:05:45 +0800

    '功能优化'

---
+++ src/widget/widget2.vue
@@ -0,0 +1,26 @@
+<template id="demo.widget2">
+    <div>demowidget</div>
+</template>
+<style id="demo-widget2" lang="less">
+
+</style>
+<script>
+    layui.define((exports) => {
+        const widget = {
+            cfg:{
+                name  : 'Demo Widget2',
+                id    : 'demo.widget2',
+                width : 50,
+                setUrl: '',
+            },
+            renderConfig() {
+
+            },
+            render() {
+                alert("widget2 init")
+            }
+        };
+
+        exports('@demo.widget2', widget)
+    })
+</script>
