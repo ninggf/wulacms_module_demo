7f63fbf1d232c64f145278c9583dfcc230ba6474
fym <373788426@qq.com>
2020-07-31 10:32:33 +0800

    '优化'

--- src/widget/widget.vue
+++ src/widget/widget.vue
@@ -1,20 +1,39 @@
 <template>
-    <div>demowidget2</div>
+    <div class="widget">
+        widget
+        <hr>
+        <p>bgcolor</p>
+    </div>
 </template>
 <style id="demo-widget" lang="less">
-
+    p{
+        background: green;
+    }
 </style>
 <script>
     layui.define((exports) => {
+        Vue.component('widget', {
+            template: `$tpl$`,
+            props: {
+            },
+            data: function() {
+                return {}
+            },
+            mounted() {
+                console.log('widget1初始化成功');
+            },
+        })
         const widget = {
-            name  : 'Demo Widget',
-            id    : 'demo.widget',
-            width : 100,
-            setUrl: '',
-            init() {
-
-            }
+            cfg:{
+                name  : 'demo.widget',
+                id    : 'demo.widget',
+                component:"widget",
+                width : 100,
+                setUrl: '',
+            },
+           
         };
+
         exports('@demo.widget', widget)
     })
 </script>
--- src/widget/widget2.vue
+++ src/widget/widget2.vue
@@ -1,24 +1,37 @@
 <template>
-    <div>demowidget2</div>
+    <div class="widget2">
+        widget2
+        <hr>
+        <p>bgcolor</p>
+    </div>
 </template>
 <style id="demo-widget2" lang="less">
-
+    p{
+        background: yellow;
+    }
 </style>
 <script>
     layui.define((exports) => {
+        Vue.component('widget2', {
+            template: `$tpl$`,
+            props: {
+            },
+            data: function() {
+                return {}
+            },
+            mounted() {
+                console.log('widget2 初始化成功');
+            },
+        })
         const widget = {
             cfg:{
-                name  : 'Demo Widget2',
+                name  : 'demo.widget2',
                 id    : 'demo.widget2',
+                component:"widget2",
                 width : 50,
                 setUrl: '',
             },
-            renderConfig() {
-
-            },
-            render() {
-                alert("widget2 init")
-            }
+           
         };
 
         exports('@demo.widget2', widget)
